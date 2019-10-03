283c226a608d61f705a13acf9a4b288aebe55f3e
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2019-10-03 11:02:08 -0300

    feat: add reset to saveCanvas script

--- saveCanvas.js
+++ saveCanvas.js
@@ -4,4 +4,11 @@ function keyPressed() {
     const date = new Date()
     saveCanvas(`${filename}-${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}@${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}.png`)
   }
+
+  /* redraw */
+  if (keyCode === RETURN) {
+    clear();
+    setup();
+    draw();
+  }
 }
