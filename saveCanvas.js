b454ba7218410027e3bef13a8fd7a57f9f9f914f
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2019-11-19 16:09:26 -0300

    fix: add filename logic to saveCanvas script

--- saveCanvas.js
+++ saveCanvas.js
@@ -1,8 +1,16 @@
 function keyPressed() {
+  const title = document.getElementsByTagName('title')
+  const titleName = title && title[0] && title[0].text ? _.camelCase(title[0].text) : ''
+  const version = document.getElementById('version')
+  const versionStr = version && version.content
+
   /* 's' letter */
   if (keyCode === 83) {
     const date = new Date()
-    saveCanvas(`${filename}-${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}@${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}.png`)
+    const dateStr = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}@${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}`
+
+    const filename = [titleName, versionStr, dateStr].filter(Boolean).join('-')
+    saveCanvas(`${filename}.png`)
   }
 
   /* redraw */
